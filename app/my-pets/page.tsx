fef3c8327ecbfe93d018b507245111709cb4cec9
virvaldev <valtteri.virtanen.dev@gmail.com>
2024-01-27 23:04:24 +0200

    MyPets-sivua ja DogForm-komponenttia.

--- app/my-pets/page.tsx
+++ app/my-pets/page.tsx
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from "react";
 import styles from './page.module.css';
 import { useUser} from "@/app/context/userContext";
+import { Page } from '@/app/petForms/dog/page';
 
 interface MessageProps {
     text: string
@@ -11,16 +12,40 @@ const Message: React.FC<MessageProps> = ({ text }) => <p className={styles.messa
 
  const MyPets: React.FC = () => {
      const { user, setUser } = useUser();
+     const [showForm, setShowForm] = useState<boolean>(false)
 
+     const handleShowForm = (): void => {
+         setShowForm((prevValue: boolean) => !prevValue)
+     }
+     console.log(showForm)
 
     return (
         <div className={styles.container}>
-            <div>
-                <button onClick={() => setUser(user => user = { ...user, hasPets: true })}>Has Pets?</button>
-                {user.hasPets ? <Message text="You have [INSERT ANIMAL COUNT] currently registered." /> : <Message text="You don't yet have registered pets." />}
+
+            <div className={styles.temporary}>
+                {user.hasPets ? <Message text="You have [INSERT ANIMAL COUNT] currently registered."/> :
+                    <Message text="You haven't registered any Pet yet."/>}
+            </div>
+
+          <button onClick={handleShowForm} className={styles.addNewPetBtn}>Add New Pet</button>
+
+            <div className={styles.addPetContainer}>
+                <h3 className={styles.addPetTitle}>
+                    Add new Pet
+                </h3>
+
+                <form className={styles.addPetForm}>
+                    <p className={styles.formTitle}>
+                        What kind of Pet Friend you want to add?
+                    </p>
+                    <select className={styles.dropdownMenu}>
+                        <option value="dog">Dog</option>
+                        <option value="cat">Cat</option>
+                    </select>
+                </form>
             </div>
         </div>
     )
-}
+ }
 
 export default MyPets;
