58cfb4a3c6f5b54b177e3e921715f0b6d28a9384
virvaldev <valtteri.virtanen.dev@gmail.com>
2024-01-28 02:28:21 +0200

    Select options and handleSubmit.

--- app/my-pets/page.tsx
+++ app/my-pets/page.tsx
@@ -8,16 +8,30 @@ interface MessageProps {
     text: string
 }
 
+interface MyPetsProps {
+    // props here if necessary
+}
+
 const Message: React.FC<MessageProps> = ({ text }) => <p className={styles.message}>{text}</p>
 
  const MyPets: React.FC = () => {
      const { user, setUser } = useUser();
-     const [showForm, setShowForm] = useState<boolean>(false)
+     const [showForm, setShowForm] = useState<boolean>(false);
+     const [selectedPet, setSelectedPet] = useState<string>("");
 
      const handleShowForm = (): void => {
          setShowForm((prevValue: boolean) => !prevValue)
      }
-     console.log(showForm)
+
+     const handleSubmit = (event: React.FormEvent<HTMLFormElement>): void => {
+         event.preventDefault();
+
+         const formData: FormData = new FormData(event.currentTarget);
+         const selectedValue: string = formData.get('petSelect') as string;
+         setSelectedPet(selectedValue);
+     }
+
+     console.log(selectedPet)
 
     return (
         <div className={styles.container}>
@@ -28,7 +42,7 @@ const Message: React.FC<MessageProps> = ({ text }) => <p className={styles.messa
             </div>
 
 
-            {showForm ? <AddNewPetsForm/> :
+            {showForm ? <AddNewPetsForm handleSubmit={handleSubmit} /> :
                 <button onClick={handleShowForm} className={styles.addNewPetBtn}>Add New Pet</button>}
 
 
@@ -38,22 +52,36 @@ const Message: React.FC<MessageProps> = ({ text }) => <p className={styles.messa
 
 export default MyPets;
 
-const AddNewPetsForm: React.FC = () => {
+interface AddNewPetsFormProps {
+    handleSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+}
+
+const AddNewPetsForm: React.FC<AddNewPetsFormProps> = ({ handleSubmit }) => {
      return (
          <div className={styles.addPetContainer}>
              <h3 className={styles.addPetTitle}>
                  Add new Pet
              </h3>
-         <form className={styles.addPetForm}>
+         <form onSubmit={handleSubmit} id='addPetForm' className={styles.addPetForm}>
              <p className={styles.formTitle}>
                  What kind of Pet Friend you want to add?
              </p>
-             <select className={styles.dropdownMenu}>
-                 <option value="dog">Dog</option>
-                 <option value="cat">Cat</option>
+             <select defaultValue='select' name='petSelect' form='addPetForm' className={styles.dropdownMenu}>
+                 <option  value="select" disabled>Select the Pet</option>
+                 <optgroup label='House Pets' >
+                     <option value="dog">Dog</option>
+                     <option value="cat">Cat</option>
+                     <option value="hamster">Hamster</option>
+                     <option value="rabbit">Rabbit</option>
+                 </optgroup>
+                 <optgroup label='Farm Animals'>
+                     <option value="horse">Horse</option>
+                     <option value="sheep">Sheep</option>
+                     <option value="chicken">Chicken</option>
+                 </optgroup>
              </select>
              <div className={styles.submitBtnContainer}>
-                 <input type='submit' className={styles.submitPetBtn} value='Next'/>
+                 <button type='submit' className={styles.submitPetBtn}>Next</button>
              </div>
 
          </form>
