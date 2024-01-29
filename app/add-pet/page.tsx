b07b2361f1b8de7edb7b0b139883529475217bfd
virvaldev <valtteri.virtanen.dev@gmail.com>
2024-01-29 21:31:08 +0200

    Paluu järkevään rakenteeseen.

--- app/add-pet/page.tsx
+++ app/add-pet/page.tsx
@@ -1,71 +1,71 @@
 'use client';
 
-import React, { useState } from 'react';
-import { useFormState } from 'react-dom';
-import { useFormStatus } from 'react-dom';
-import { addPet} from "@/app/actions";
+
 import styles from './page.module.css';
+import React, { useState } from 'react';
+import { useFormState, useFormStatus } from 'react-dom';
 import { useRouter } from "next/navigation";
 
-const initialState: {type: string} = {
-    type: "",
-}
 
 /* SUBMIT BUTTON */
 function SubmitBtn(): JSX.Element {
-    const { pending: boolean} = useFormStatus();
-
+    const { pending} = useFormStatus();
     return (
-        <button type='submit' aria-disabled={pending}>
-            Add Pet
+        <button type='submit' aria-disabled={pending} className={styles.submitBtn}>
+            {pending ? "Adding..." : "Add"}
+
         </button>
     );
 }
 
+function Form({ action, selectedPet }): React.JSX.Element {
+    return (
+        <form action={action} id='addPetForm' className={styles.addPetForm}>
+            <p className={styles.formTitle}>
+                What kind of Pet Friend you want to add?
+            </p>
 
-/* ADD-PET PAGE */
-const AddPetForm: React.FC = () => {
-    const [state, formAction] = useFormState(addPet, initialState)
+            <select value={selectedPet} name='petSelect' form='addPetForm' className={styles.dropdownMenu}>
+                <option value="select" disabled>Select the Pet</option>
+                <optgroup label='House Pets'>
+                    <option value="dog">Dog</option>
+                    <option value="cat">Cat</option>
+                    <option value="hamster">Hamster</option>
+                    <option value="rabbit">Rabbit</option>
+                </optgroup>
+                <optgroup label='Farm Animals'>
+                    <option value="horse">Horse</option>
+                    <option value="sheep">Sheep</option>
+                    <option value="chicken">Chicken</option>
+                </optgroup>
+            </select>
+
+            <div className={styles.submitBtnContainer}>
+                <SubmitBtn />
+            </div>
+        </form>
+    )
+}
+
+const AddPet: React.FC = () => {
     const router = useRouter(); // Initializes useRouter
     const [selectedPet, setSelectedPet] = useState<string>(""); // Initializes user-selected Pet
 
-    // Handles change of selected pet
-    const handlePetChange = (event: React.ChangeEvent<HTMLSelectElement>): void => {
-        const pet: string = event.target.value; // Crabs the selection
-        setSelectedPet(pet); // Re-valuates 'selectedPet'-state
-    };
+    const submitForm = async (query: string): Promise<void>=> {
+            await new Promise((res) => setTimeout(res, 1000))
+            console.log("submitting...")
+    }
 
     return (
         <div className={styles.addPetContainer}>
+            {selectedPet && <p className={styles.selectionTxt}>You've selected {selectedPet}.</p>}
             <h3 className={styles.addPetTitle}>
                 Add new Pet
             </h3>
-            <form action={formAction} id='addPetForm' className={styles.addPetForm}>
-                    <p className={styles.formTitle}>
-                        What kind of Pet Friend you want to add?
-                    </p>
-
-                    <select defaultValue='select' name='petSelect' form='addPetForm' className={styles.dropdownMenu}>
-                        <option  value="select" disabled>Select the Pet</option>
-                        <optgroup label='House Pets' >
-                            <option value="dog">Dog</option>
-                            <option value="cat">Cat</option>
-                            <option value="hamster">Hamster</option>
-                            <option value="rabbit">Rabbit</option>
-                        </optgroup>
-                        <optgroup label='Farm Animals'>
-                            <option value="horse">Horse</option>
-                            <option value="sheep">Sheep</option>
-                            <option value="chicken">Chicken</option>
-                        </optgroup>
-                    </select>
-
-                    <div className={styles.submitBtnContainer}>
-                        <SubmitBtn />
-                    </div>
-            </form>
+            <Form action={submitForm} />
         </div>
     )
 }
 
-export default AddPetForm;
+export default AddPet;
+
